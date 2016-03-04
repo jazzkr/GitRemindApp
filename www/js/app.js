33a112fc1c4ebd9c288b5931bac9a56d1a142af8
Krisztian Kurucz <kkurucz@uwaterloo.ca>
2016-03-04 08:28:02 -0500

    Cleaned up controllers and GUI

--- www/js/app.js
+++ www/js/app.js
@@ -1,48 +1,53 @@
 var ged = angular.module('ionicApp', ['ionic']);
 
-ged.config(function($stateProvider, $urlRouterProvider) {
-  $stateProvider
-    .state('connect', {
-      url: '/connect',
-      templateUrl: 'templates/connect.html',
-      controller: 'ConnectCtrl'
-    })
-    .state('tabs', {
-      url: '/tab',
-      abstract: true,
-      templateUrl: 'templates/tabs.html'
-    })
-    .state('tabs.home', {
-      url: '/home',
-      views: {
-        'home-tab': {
-          templateUrl: 'templates/home.html'
-        }
-      }
-    })
-    .state('tabs.settings', {
-      url: '/settings',
-      views: {
-        'settings-tab': {
-          templateUrl: 'templates/settings.html',
-          controller: 'SettingsTabCtrl'
-        }
-      }
-    });
+ged.config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+        .state('connect', {
+            url: '/connect',
+            templateUrl: 'templates/connect.html',
+            controller: 'UserCtrl'
+        })
+        .state('tabs', {
+            url: '/tab',
+            abstract: true,
+            templateUrl: 'templates/tabs.html'
+        })
+        .state('tabs.home', {
+            url: '/home',
+            views: {
+                'home-tab': {
+                    templateUrl: 'templates/home.html',
+                    controller: 'UserCtrl'
+                }
+            }
+        })
+        .state('tabs.settings', {
+            url: '/settings',
+            views: {
+                'settings-tab': {
+                    templateUrl: 'templates/settings.html',
+                    controller: 'UserCtrl'
+                }
+            }
+        });
 
-   $urlRouterProvider.otherwise('/connect');
+    $urlRouterProvider.otherwise('/connect');
 });
 
-ged.controller('ConnectCtrl', function($scope, $state) {
-  $scope.connect = function(username) {
-    console.log('Connect', username);
-    $state.go('tabs.home');
-  };
-});
-
-ged.controller('SettingsTabCtrl', function($scope, $state) {
-  $scope.disconnect= function(username) {
-    console.log('Disconnect', username);
-    $state.go('tabs.connect');
-  };
+ged.controller('UserCtrl', function ($scope, $state) {
+                                                      
+    $scope.connect = function (user) {
+        if(user == undefined) {
+            console.log("No username entered!");
+            alert("No username entered!");
+            return;
+        }
+        console.log('Connect', user);
+        $state.go('tabs.home');
+    };
+    
+    $scope.disconnect = function (user) {
+        console.log('Disconnect', user);
+        $state.go('connect');
+    };
 });
